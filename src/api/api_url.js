1ff9661c93b97216b0e9f202c17456c8ff9d22db
silence25111999 <siddhantguha5@gmail.com>
2025-01-17 12:32:34 +0530

    changes

--- src/api/api_url.js
+++ src/api/api_url.js
@@ -1,3 +1,3 @@
-export const baseurl="https://dummyjson.com"
-export const endurl="/products"
-export const catagoryurl="/category-list"
+export const baseurl="https://dummyjson.com/"
+export const endurl="products"
+export const catagoryurl="category-list"
